8220784562f3ddd2a660de64878d098cfa6bb82e
Nikola <88djony@gmail.com>
2021-05-08 17:38:15 -0700

    add license permissions

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -13,6 +13,7 @@ if(license === 'MIT') {
 }
 }
 
+// licenses links
 function renderLinkLicense(license) {
     if(license === 'MIT') {
         return `[MIT License](https://opensource.org/licenses/MIT)`
@@ -25,3 +26,19 @@ function renderLinkLicense(license) {
     } else if(license === 'none')
     return ``
 }
+
+// license permissions
+function renderSections(license) {
+    if(license === 'MIT') {
+        return `A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+    } else if(license === 'Apache') {
+        return `A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+    } else if(license === 'GNU') {
+        return `Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.`
+    } else if(license === 'ISC') {
+        return `A permissive license lets people do anything with your code with proper attribution and without warranty. The ISC license is functionally equivalent to the BSD 2-Clause and MIT licenses, removing some language that is no longer necessary.`
+    } else if(license === 'none') {
+        return ``
+      }
+}
+
